21582d30753d7ed4b8e6bb945fe68e1e74ea5829
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-30 00:54:20 +0400

    Revert

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { app } from "./server.ts";
-import { env } from "#env.ts";
-
-if (env.NODE_ENV !== "production") {
-  const port = env.PORT ?? "4000";
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}
+// import { env } from "#env.ts";
+//
+// if (env.NODE_ENV !== "production") {
+//   const port = env.PORT ?? "4000";
+//   app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+//   });
+// }
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import { env } from "#env.ts";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: env.CORS_ORIGIN,
-    credentials: true
-  })
-);
+// app.use(
+//   cors({
+//     origin: env.CORS_ORIGIN,
+//     credentials: true
+//   })
+// );
 
 app.get("/", (_req, res) => {
   res.json({ message: "Hello from Express on Vercel!!" });
