828aea8d2fadd3b3fa2f69a33276fa1c2d4bf7fa
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-30 01:00:30 +0400

    Revert 2

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,21 @@
 import express from "express";
-// import cors from "cors";
-//
-// import { env } from "#env.ts";
+import cors from "cors";
+
+import { env } from "#env.ts";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: env.CORS_ORIGIN,
-//     credentials: true
-//   })
-// );
+app.use(
+  cors({
+    origin: env.CORS_ORIGIN,
+    credentials: true
+  })
+);
 
 app.get("/", (_req, res) => {
   res.json({ message: "Hello from Express on Vercel!!" });
 });
 
 export { app };
+
+export default app;
