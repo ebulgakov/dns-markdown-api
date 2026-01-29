f415b1db8ab5e7edfcae90815c7d0771b9bd25c2
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-30 01:03:31 +0400

    Revert 3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { app } from "./server.ts";
 import { env } from "#env.ts";
 
-if (env.NODE_ENV !== "production") {
-  const port = env.PORT ?? "4000";
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
-}
+const port = env.PORT;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
 
 export default app;
