2cf692497c9c13a36c38bf75c834d4330d1110d6
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-30 01:21:24 +0400

    Add error

--- env.ts
+++ env.ts
@@ -17,7 +17,7 @@ const envSchema = z.object({
   PORT: z.string().default("4000"),
   NODE_ENV: z.enum(["development", "production", "test"]).default("development"),
 
-  DATABASE_URL: z.string().startsWith("mongodb+srv://"),
+  DATABASE_URL: z.string().default("localhost"),
 
   // CORS
   CORS_ORIGIN: z
