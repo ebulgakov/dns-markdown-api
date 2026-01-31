4e108520d5443dc3eba9ecd07ef48056fc522e97
Eugene Bulgakov <zoom81@yandex.ru>
2026-01-31 13:43:32 +0400

    Update analysis page

--- src/analysis/index.ts
+++ src/analysis/index.ts
@@ -2,8 +2,13 @@ import { Router } from "express";
 
 import { cacheAdd, cacheGet } from "../../cache";
 import { AnalysisDiff } from "../../db/models/analysis-diff";
+import { Pricelist } from "../../db/models/pricelist.ts";
 
-import type { AnalysisDiff as AnalysisDiffType } from "../../types/analysis-diff";
+import type {
+  AnalysisDiff as AnalysisDiffType,
+  AnalysisDiffReport
+} from "../../types/analysis-diff";
+import type { PriceList as PriceListType, PriceListsArchiveCount } from "../../types/pricelist.ts";
 
 const router = Router();
 
@@ -24,6 +29,73 @@ router.get("/last-diff", async (req, res, next) => {
     await cacheAdd<AnalysisDiffType>(key, diff, { ex: 60 * 60 * 24 }); // 24 hours
 
     res.json(diff);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/all-diffs", async (req, res, next) => {
+  try {
+    const city = req.query.city as string;
+    if (!city) return res.status(400).send("city is required");
+
+    const key = `daily:analysis:all:${String(city)}`;
+    const cached = await cacheGet<AnalysisDiffReport[]>(key);
+    if (cached) return res.json(cached);
+
+    const diffs = (await AnalysisDiff.find(
+      { city },
+      {},
+      { sort: { dateAdded: -1 }, limit: 30 }
+    )) as AnalysisDiffType[];
+
+    const report: AnalysisDiffReport[] = [];
+
+    diffs.forEach(diff => {
+      report.push({
+        city: city,
+        dateAdded: diff.dateAdded,
+        newItems: diff.newItems.length,
+        removedItems: diff.removedItems.length,
+        changesPrice: diff.changesPrice.length,
+        changesProfit: diff.changesProfit.length
+      });
+    });
+
+    await cacheAdd<AnalysisDiffReport[]>(key, report, { ex: 60 * 60 * 24 }); // 24 hours
+
+    res.json(report);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/products-count", async (req, res, next) => {
+  try {
+    const city = req.query.city as string;
+    if (!city) return res.status(400).send("city is required");
+
+    const key = `daily:analysis:products-count:${String(city)}`;
+    const cached = await cacheGet<PriceListsArchiveCount[]>(key);
+    if (cached) return res.json(cached);
+
+    const priceLists = (await Pricelist.find({ city }, {}, { sort: { updatedAt: -1 }, limit: 30 })
+      .lean()
+      .exec()) as PriceListType[];
+    if (!priceLists) return res.status(404).send("No archived price lists found");
+
+    const productsCountByDates: PriceListsArchiveCount[] = priceLists
+      .map(priceList => {
+        return {
+          date: priceList.createdAt,
+          count: priceList.positions.flatMap(position => position.items).length
+        };
+      })
+      .reverse();
+
+    await cacheAdd<PriceListsArchiveCount[]>(key, productsCountByDates, { ex: 60 * 60 * 24 }); // 24 hours
+
+    res.json(productsCountByDates);
   } catch (error) {
     next(error);
   }
