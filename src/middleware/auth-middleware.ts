e7c8355f5d3a91b85fcfd08922d255638620fac2
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-03 10:36:45 +0400

    Add secret key

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -2,8 +2,8 @@ import { env } from "@src/env";
 
 import type { NextFunction, Request, Response } from "express";
 
-export const serviceMiddleware = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers["authorization"];
+export const authServiceMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.header("authorization");
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ error: "Missing or invalid Authorization header" });
@@ -14,6 +14,16 @@ export const serviceMiddleware = (req: Request, res: Response, next: NextFunctio
   // Just in case if token is empty string and API_SERVICE_KEY also empty string
   if (!token || token !== env.API_SERVICE_KEY) {
     return res.status(401).json({ error: "Unauthorized Service" });
+  }
+
+  next();
+};
+
+export const authPublicMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const secret = req.header("X-Internal-API-Secret");
+
+  if (!secret || secret !== env.API_SECRET_KEY) {
+    return res.status(403).json({ error: "Forbidden: Invalid API secret" });
   }
 
   next();
