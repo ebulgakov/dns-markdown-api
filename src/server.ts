372de8ff67d5bb642cf6e017e7ccdf5645a1d565
Eugene Bulgakov <zoom81@yandex.ru>
2026-02-08 15:06:00 +0400

    Add new limits

--- src/server.ts
+++ src/server.ts
@@ -34,9 +34,9 @@ app.use(
 
 app.use(helmet());
 
-// Set limit to 20MB
-app.use(express.json({ limit: "20mb" }));
-app.use(express.urlencoded({ limit: "20mb", extended: true }));
+// Set limit to 30MB
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
 // Use a raw body parser for the Cleak webhook route
 app.use("/clerk/create-user", express.raw({ type: "application/json" }));
